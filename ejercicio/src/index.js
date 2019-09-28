ecdeb8dfbdf693187e73125a706822b919de6551
Franco Daniel Demetrio <francodemetrio87@gmail.com>
2019-09-28 11:27:08 -0300

    commit002

--- ejercicio/src/index.js
+++ ejercicio/src/index.js
@@ -4,9 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>PESTAÑAS CON REACT</h1>, document.getElementById('header'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const asignaturas = React.createFactory('li');
+
+const materia1 = asignaturas({ id: 'item1' }, 'Física');
+const materia2 = asignaturas({ id: 'item2' }, 'Química');
+const materia3 = asignaturas({ id: 'item3' }, 'Biología');
+const materia4 = asignaturas({ id: 'item4' }, 'Inglés');
+const materia5 = asignaturas({ id: 'item5' }, 'Sociales');
+const materia6 = asignaturas({ id: 'item6' }, 'Historia');
+const materia7 = asignaturas({ id: 'item7' }, 'Deporte');
+
+const elements = [materia1, materia2, materia3, materia4, materia5, materia6, materia7];
+
+const listOfasignaturas = React.createElement('lu', { id: 'lista' }, elements);
+
+ReactDOM.render(listOfasignaturas, document.getElementById('list'));
+
+
+
 serviceWorker.unregister();
